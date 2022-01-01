fb856d5ea8f93b1dcd19796ac0e79355f7a81e17
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2022-01-01 11:09:08 +0530

    Create GroupedAnagrams.cpp

---
+++ GoldmanSachs/GroupedAnagrams.cpp
@@ -0,0 +1,20 @@
+# Given an array of strings, return all groups of strings that are anagrams.
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        unordered_map<string,vector<string>>AnagMap;
+        string temp;
+        for(int i = 0 ; i<string_list.size();i++){
+            temp= string_list[i];
+            sort(string_list[i].begin(),string_list[i].end());
+            AnagMap[string_list[i]].push_back(temp);
+        }
+        vector<vector<string>> sol;
+        for(auto i=AnagMap.begin();i!=AnagMap.end();i++){
+            sol.push_back(i->second);
+        }
+        return sol;
+        
+    }
+};
