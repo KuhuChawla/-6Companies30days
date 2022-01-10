9df2b075e968a6d6bf20699b48b157765ed618c0
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2022-01-10 10:45:25 +0530

    Create nthOrderOFstair.cpp

---
+++ Amazon/nthOrderOFstair.cpp
@@ -0,0 +1,15 @@
+
+//Without Dp
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        long long ans;
+        ans = (m/2)+1;
+        return ans;
+    }
+};
