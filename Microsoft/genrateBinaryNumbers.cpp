b2630deeb2c8fc15103eb8942496639fe6be9975
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2022-01-14 18:25:32 +0530

    Create genrateBinaryNumbers.cpp

---
+++ Microsoft/genrateBinaryNumbers.cpp
@@ -0,0 +1,16 @@
+vector<string> generate(int N)
+{
+	vector<string> ans;
+	string s = "";
+	for(int i=1;i<=N;i++){
+	    int a =i;
+	    while(a){
+	        int b = a%2;
+	        s =to_string(b)+s;
+	        a/=2;
+	    }
+	    ans.push_back(s);
+	    s="";
+	}
+	return ans;
+}
