044fa6f5b4e722c9b05a69eda4b7e1a30ecc333b
KuhuChawla <72125043+KuhuChawla@users.noreply.github.com>
2022-01-29 12:45:59 +0530

    Create kthLargestIntegerInArray.cpp

---
+++ Walmart/kthLargestIntegerInArray.cpp
@@ -0,0 +1,19 @@
+struct helper{
+        bool operator()(string &s1, string &s2){
+            if(s1.size()!=s2.size()){
+                return s1.size()<s2.size();
+            }
+            return s1<s2;
+        }
+    };
+    
+    string kthLargestNumber(vector<string>& nums, int k) {
+        priority_queue<string,vector<string>, helper>pq;
+        for(auto i:nums){
+            pq.push(i);
+        }
+        while(--k){
+            pq.pop();
+        }
+        return pq.top();
+    }
